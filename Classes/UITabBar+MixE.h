a2552ec1ef2d82c0cee2573892f14d941e29836d
eric <longminxiang@163.com>
2019-03-13 16:21:56 +0800

    add tabbar extention

---
+++ Classes/UITabBar+MixE.h
@@ -0,0 +1,12 @@
+//
+//  UITabBar+MixE.h
+//
+//  Created by Eric Lung on 2018/11/30.
+//  Copyright © 2018年 YOOEE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixTabBarBugFix : NSObject
+
+@end
